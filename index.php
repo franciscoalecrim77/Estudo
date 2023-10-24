811679c5fcd6797a9f931b3e4023f56a77964a37
franciscoalecrim77 <francisco_crim@hotmail.com>
2023-10-24 12:01:57 -0300

    alteracao de teste 1

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
 
-include ("conn.php");
-
-$pdo = conectar();
-
-print_r($pdo);
-
-$id = 2;
-
-$stmt = $pdo->prepare('SELECT * FROM Clientes where id_clientes = ' . $id);
-$stmt->execute();
-
-$resultado = $stmt->fetchAll();
-
-foreach ($resultado as $key) {
-    $consulta = $key[1];
-    echo "<pre>";
-    print_r($consulta);
-}
-
-
+phpinfo();
 
 
 ?>
